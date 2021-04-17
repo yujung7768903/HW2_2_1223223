052d4b2edae53d0086a838240b9f11a0e3f30af7
yujung7768903 <yu-jung31476@naver.com>
2021-04-17 21:59:52 +0900

    모든 기능 구현 완료

---
+++ app/src/main/java/com/example/hw2_2_1223223/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.hw2_2_1223223;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.graphics.Matrix;
+
+public class MainActivity extends AppCompatActivity {
+    Button rotateButton;
+    ImageView rotateImage;
+    int mDegree = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rotateButton = (Button) findViewById(R.id.rotate_button);
+        rotateImage = (ImageView) findViewById(R.id.rotate_image);
+
+        rotateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rotation(mDegree + 10);
+            }
+        });
+    }
+
+    public void rotation(int d) {
+        RotateAnimation ra = new RotateAnimation(mDegree, d, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        ra.setDuration(200);
+        ra.setFillAfter(true);
+        rotateImage.startAnimation(ra);
+        mDegree = d;
+    }
+}
